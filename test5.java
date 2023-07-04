6496ffb40dc5b00be249c39f9a08d42d457f39d1
Harsh Anand <118230039+harsh092003@users.noreply.github.com>
2023-07-04 23:49:33 +0530

    Add files via upload

---
+++ test5.java
@@ -0,0 +1,14 @@
+public class test5 {
+    public static void main(String[] args) {
+        String name="Harsh";
+        int noSubj=5;
+        double perct=94.3;
+
+        System.out.println("branch Name: " + name);
+        System.out.println("No. of Subject: " + noSubj);
+        System.out.println("Total percentage: " + perct );
+
+    }
+}
+
+
